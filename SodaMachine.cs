232a9d5cae122cd22df3075683030c53a070abff
Lewis Vine <lew.vine@gmail.com>
2024-02-08 14:56:15 -0500

    Refactored Soda class constructor

--- SodaMachine.cs
+++ SodaMachine.cs
@@ -10,16 +10,29 @@ namespace SodaMachine
     {
         public List<Soda> sodas;
         public List<Coin> coins;
-        public List<Coin> insertedCoins;
 
         public SodaMachine()
         {
-            
-        }
+            //Add coins
+            this.coins = new List<Coin>();
+            Coin quarters = new Coin("Quarter", .25, 20);
+            Coin dimes = new Coin("Dime", .1, 10);
+            Coin nickels = new Coin("Nickel", .05, 20);
+            Coin pennys = new Coin("Penny", .01, 50);
+            this.coins.Add(quarters);
+            this.coins.Add(dimes);
+            this.coins.Add(nickels);
+            this.coins.Add(pennys);
 
-        public void SetStartingInventory() 
-        {
-        
+            //Add sodas
+            this.sodas = new List<Soda>();
+            Soda orange = new Soda("Orange", .06, 30);
+            Soda cola = new Soda("Cola", .35, 30);
+            Soda rootBeer = new Soda("Root Beer", .60, 30);
+            this.sodas.Add(orange);
+            this.sodas.Add(cola);
+            this.sodas.Add(rootBeer);
+
         }
     }
 }
--- Wallet.cs
+++ Wallet.cs
@@ -14,102 +14,26 @@ namespace SodaMachine
         public Wallet()
         {
             this.change = new List<Coin>();
-            FillWallet();
+            Coin quarters = new Coin("Quarter", .25, 20);
+            Coin dimes = new Coin("Dime", .1, 10);
+            Coin nickels = new Coin("Nickel", .05, 20);
+            Coin pennys = new Coin("Penny", .01, 50);
+            this.change.Add(quarters);
+            this.change.Add(dimes);
+            this.change.Add(nickels);
+            this.change.Add(pennys);
 
         }
 
-        public void FillWallet()
-        {
-            Quarter quarter1 = new Quarter();
-            Quarter quarter2 = new Quarter();
-            Quarter quarter3 = new Quarter();
-            Quarter quarter4 = new Quarter();
-            Quarter quarter5 = new Quarter();
-            Quarter quarter6 = new Quarter();
-            Quarter quarter7 = new Quarter();
-            Quarter quarter8 = new Quarter();
-            Quarter quarter9 = new Quarter();
-            Quarter quarter10 = new Quarter();
-            Dime dime1 = new Dime();
-            Dime dime2 = new Dime();
-            Dime dime3 = new Dime();
-            Dime dime4 = new Dime();
-            Dime dime5 = new Dime();
-            Dime dime6 = new Dime();
-            Dime dime7 = new Dime();
-            Dime dime8 = new Dime();
-            Dime dime9 = new Dime();
-            Dime dime10 = new Dime();
-            Nickel nickel1 = new Nickel();
-            Nickel nickel2 = new Nickel();
-            Nickel nickel3 = new Nickel();
-            Nickel nickel4 = new Nickel();
-            Nickel nickel5 = new Nickel();
-            Nickel nickel6 = new Nickel();
-            Nickel nickel7 = new Nickel();
-            Nickel nickel8 = new Nickel();
-            Nickel nickel9 = new Nickel();
-            Nickel nickel10 = new Nickel();
-            Penny penny1 = new Penny();
-            Penny penny2 = new Penny();
-            Penny penny3 = new Penny();
-            Penny penny4 = new Penny();
-            Penny penny5 = new Penny();
-            Penny penny6 = new Penny();
-            Penny penny7 = new Penny();
-            Penny penny8 = new Penny();
-            Penny penny9 = new Penny();
-            Penny penny10 = new Penny();
-            this.change.Add(quarter1);
-            this.change.Add(quarter2);
-            this.change.Add(quarter3);
-            this.change.Add(quarter4);
-            this.change.Add(quarter5);
-            this.change.Add(quarter6);
-            this.change.Add(quarter7);
-            this.change.Add(quarter8);
-            this.change.Add(quarter9);
-            this.change.Add(quarter10);
-            this.change.Add(dime1);
-            this.change.Add(dime2);
-            this.change.Add(dime3);
-            this.change.Add(dime4);
-            this.change.Add(dime5);
-            this.change.Add(dime6);
-            this.change.Add(dime7);
-            this.change.Add(dime8);
-            this.change.Add(dime9);
-            this.change.Add(dime10);
-            this.change.Add(nickel1);
-            this.change.Add(nickel2);
-            this.change.Add(nickel3);
-            this.change.Add(nickel4);
-            this.change.Add(nickel5);
-            this.change.Add(nickel6);
-            this.change.Add(nickel7);
-            this.change.Add(nickel8);
-            this.change.Add(nickel9);
-            this.change.Add(nickel10);
-            this.change.Add(penny1);
-            this.change.Add(penny2);
-            this.change.Add(penny3);
-            this.change.Add(penny4);
-            this.change.Add(penny5);
-            this.change.Add(penny6);
-            this.change.Add(penny7);
-            this.change.Add(penny8);
-            this.change.Add(penny9);
-            this.change.Add(penny10);
-        }
 
-        public double GetTotalChange()
-        {
-            double totalChange = 0;
-            foreach(Coin coin in change)
-            {
-                totalChange += coin.Value;
-            }
-            return totalChange;
-        }
+        //public double GetTotalChange()
+        //{
+        //    double totalChange = 0;
+        //    foreach(Coin coin in change)
+        //    {
+        //        totalChange += coin.Value;
+        //    }
+        //    return totalChange;
+        //}
     }
 }
