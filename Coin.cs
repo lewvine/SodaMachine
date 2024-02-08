078802a6c3896541a26069facbfa7d85bc544db7
Lewis Vine <lew.vine@gmail.com>
2024-02-08 13:27:24 -0500

    Made classes

---
+++ Coin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    internal class Coin
+    {
+        private double value;
+
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
